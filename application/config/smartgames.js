b8cc063bcfa33cfc0679394360cb98e4c181a7cd
VanesStalkers <ivan.mironov@bk.ru>
2026-03-25 16:12:37 +0300

    (+) config.smartgames.telegramEnabled

--- application/config/smartgames.js
+++ application/config/smartgames.js
@@ -1,3 +1,4 @@
 ({
   appCode: 'smartgamesPortal',
+  telegramEnabled: false,
 });
--- application/domain/lobby/start.js
+++ application/domain/lobby/start.js
@@ -1,53 +1,54 @@
 async () => {
   if (application.worker.id === 'W1') {
-    db.redis.handlers.afterStart({ flushDB: async () => {
-      await db.redis.flushDb(); // тут список игроков online и список загруженных игр
-    } });
+    db.redis.handlers.afterStart({
+      flushDB: async () => {
+        await db.redis.flushDb(); // тут список игроков online и список загруженных игр
+      },
+    });
 
-    db.mongo.handlers.afterStart({ initTelegram: async () => {
-    /*
+    db.mongo.handlers.afterStart({
+      initTelegram: async () => {
+        if (config.smartgames.telegramEnabled) {
+          const TelegramBot = npm['node-telegram-bot-api'];
+          const bot = new TelegramBot(config.telegram.botToken, { polling: true });
+          await bot
+            .setMyCommands([
+              {
+                command: '/games',
+                description: 'Список игр',
+              },
+              {
+                command: '/watch',
+                description: 'Отслеживать новый игры',
+              },
+            ])
+            .then(() => {
+              console.log('TelegramBot started.');
+            })
+            .catch((err) => {
+              console.error('!!! TelegramAPI setMyCommands error');
+              throw err?.message;
+            });
+          bot.onText(/\/watch(@|\s|\b)(?!\w)/, async (msg, match) => {
+            const {
+              from: { id, username, is_bot },
+            } = msg;
 
-      try {
-        const { Midjourney } = npm.midjourney;
-        const client = new Midjourney(config.midjourney);
-        await client.init();
-        lobby.midjourneyClient(client);
-      } catch (err) {
-        console.log(err);
-      }
+            if (is_bot) return;
+            await lobby.startWatching({ telegramId: id, telegramUsername: username });
+          });
+          lobby.telegramBot(bot);
+        }
 
-      return; // отключил до отладки
-
-      const TelegramBot = npm['node-telegram-bot-api'];
-      const bot = new TelegramBot(config.telegram.botToken, { polling: true });
-      await bot
-        .setMyCommands([
-          {
-            command: '/games',
-            description: 'Список игр',
-          },
-          {
-            command: '/watch',
-            description: 'Отслеживать новый игры',
-          },
-        ])
-        .then(() => {
-          console.log('TelegramBot started.');
-        })
-        .catch((err) => {
-          console.error('!!! TelegramAPI setMyCommands error');
-          throw err?.message;
-        });
-      bot.onText(/\/watch(@|\s|\b)(?!\w)/, async (msg, match) => {
-        const {
-          from: { id, username, is_bot },
-        } = msg;
-
-        if (is_bot) return;
-        await lobby.startWatching({ telegramId: id, telegramUsername: username });
-      });
-      lobby.telegramBot(bot);
-    */
-    } });
+        // try {
+        //   const { Midjourney } = npm.midjourney;
+        //   const client = new Midjourney(config.midjourney);
+        //   await client.init();
+        //   lobby.midjourneyClient(client);
+        // } catch (err) {
+        //   console.log(err);
+        // }
+      },
+    });
   }
 };
