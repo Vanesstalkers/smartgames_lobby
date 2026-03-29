065590f94d0245fe2eddf90d8a931df020693b06
VanesStalkers <ivan.mironov@bk.ru>
2026-03-29 17:18:05 +0300

    rename lobby class (second iteration)

---
+++ application/domain/lobby/Class.js
@@ -0,0 +1,75 @@
+(class SmartGamesLobby extends lib.lobby.Class() {
+  gameServers = {};
+  gameServerChannels = {};
+
+  #telegramBot;
+  #midjourneyClient;
+
+  constructor(data, config = {}) {
+    super(data, { ...config, chatEnabled: false });
+  }
+
+  updateRankings() {
+    // do nothing
+  }
+  checkRatings(){
+    // do nothing
+  }
+
+  async processData(data, broadcasterChannelName) {
+    const lobbyMap = data.lobby;
+    const serviceCode = this.gameServerChannels?.[broadcasterChannelName]?.code;
+
+    if (lobbyMap) {
+      for (const [id, lobby] of Object.entries(lobbyMap)) {
+        if (lobby.games) {
+          this.set({ games: lobby.games });
+          this.checkGameStatuses();
+        }
+        if (lobby.rankings && serviceCode) {
+          // Игровой сервис присылает плоский rankings (без appCode),
+          // в портале храним с уровнем сервиса: rankings[serviceCode] = ...
+          this.set({ rankings: { [serviceCode]: lobby.rankings } });
+        }
+        if (lobby.rankingsUsersTop) this.set({ rankingsUsersTop: lobby.rankingsUsersTop });
+      }
+    }
+
+    delete data.lobby;
+    await super.processData(data, broadcasterChannelName);
+  }
+
+  telegramBot(bot) {
+    if (!bot) return this.#telegramBot;
+    this.#telegramBot = bot;
+  }
+  midjourneyClient(client) {
+    if (!client) return this.#midjourneyClient;
+    this.#midjourneyClient = client;
+  }
+
+  async gameServerConnected({ code, channelName, ...serverData }) {
+    const gameServers = { [code]: serverData };
+    const gameServerChannels = { [channelName]: { code } };
+    this.set({ gameServers, gameServerChannels }, { removeEmptyObject: true });
+
+    await this.saveChanges();
+    // Помимо списка игр (games) нужно получать ещё и рассчитанные top-рейтинги (rankings)
+    await this.subscribe(channelName, { rule: 'fields', fields: ['games', 'rankings', 'rankingsUsersTop'] });
+  }
+
+  getGameConfig({ gameCode, gameType, gameConfig }) {
+    const {
+      [gameCode]: {
+        games: {
+          [gameType]: {
+            items: {
+              [gameConfig]: { teamsCount, playerCount },
+            },
+          },
+        },
+      },
+    } = this.gameServers;
+    return { teamsCount, playerCount };
+  }
+});
